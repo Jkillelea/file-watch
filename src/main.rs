dcc91222d04ab35de66a84a33b6057e423681acf
Jacob Killelea <Jkillelea@protonmail.ch>
2017-09-22 11:20:27 -0600

    make it prettier

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use std::process::Command;
 fn main() {
     let command_name = match env::args().nth(1) {
         Some(n) => n,
-        None => panic!("No Command Name!"),
+        None    => panic!("No Command Name!"),
     };
     let filename = match env::args().nth(2) {
         Some(f) => f,
@@ -25,7 +25,6 @@ fn main() {
         if mtime != last_mtime {
             last_mtime = mtime;
             Command::new(&command_name) // reference because 'use of moved value' - we take the value on the first iteration and then it'd be gone
-                    // .arg("-file-line-error") // sane error messages
                     .arg(&filename)
                     .spawn()
                     .expect("Command pdflatex spawn failed");
